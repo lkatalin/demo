0ef38a29d860ed72fcafe4c189bfbfc55fe7e141
Lily S <lsturman@redhat.com>
2019-09-08 18:37:07 -0400

    added some full sentence comments

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -4,29 +4,33 @@ use std::net::{TcpListener, TcpStream};
 fn main() {
     println!("Daemon listening for attestation request on port 1034... ");
 
-    // handle each incoming connection from a tenant
+    // The attestation daemon handles each incoming connection from a tenant. The tenant, by
+    // connecting, is requesting an attestation of the enclave.
     for stream_client in TcpListener::bind("localhost:1034").unwrap().incoming() {
         let mut stream_client = stream_client.unwrap();
 
-        // connect to enclave
+        // The attestation daemon connects to the enclave.
         let mut enclave_cnx = TcpStream::connect("localhost:1032").unwrap();
 
-        // send QE targetinfo to enclave
+        // The attestation daemon retrieves the Quoting Enclave's Target Info and
+        // sends the Quoting Enclave's Target Info to the enclave. This Target Info will be
+        // used as the target for the enclave's attestation Report.
         let qe_ti = dcap_ql::target_info().expect("Could not retrieve QE target info.");
         enclave_cnx.write(&qe_ti.as_ref()).unwrap();
 
-        // receive report from enclave
+        // The attestation daemon receives the Report back from the attesting enclave.
         let mut report_buf = [0; sgx_isa::Report::UNPADDED_SIZE];
         enclave_cnx
             .read_exact(&mut report_buf)
             .expect("Could not read report");
         let report = sgx_isa::Report::try_copy_from(&report_buf).unwrap();
 
-        // get a quote from QE for the enclave's report
+        // The attestation daemon gets a Quote from the Quoting Enclave for the Report. This
+        // verifies the Report as genuine according to the platform's Quoting Enclave. 
         let quote = dcap_ql::quote(&report).expect("Could not generate quote.");
         println!("\nQuote successfully generated...");
 
-        // send quote to tenant
+        // The attestation daemon sends the Quote to the tenant.
         stream_client.write(&quote).unwrap();
     }
 }
--- intel-sgx/attestation-enclave/src/main.rs
+++ intel-sgx/attestation-enclave/src/main.rs
@@ -5,19 +5,24 @@ use std::net::TcpListener;
 fn main() {
     println!("\nListening on port 1032....\n");
 
-    // handle each incoming connection from attestation daemon
+    // This enclave handles each incoming connection from attestation daemon.
     for stream in TcpListener::bind("localhost:1032").unwrap().incoming() {
         let mut stream = stream.unwrap();
 
-        // receive QE identity (as TargetInfo) from daemon
+        // This enclave receives the identity of the Quoting Enclave from the
+        // attestation daemon, in the form of a TargetInfo structure. The 
+        // TargetInfo contains the measurement and attribute flags of the
+        // Quoting Enclave.
         let mut buf = [0; Targetinfo::UNPADDED_SIZE];
         stream.read_exact(&mut buf).unwrap();
         let qe_id = Targetinfo::try_copy_from(&buf).unwrap();
 
-        // create attestation report for QE as Target
+        // This enclave creates a Report attesting its identity, with the Quoting
+        // Enclave as the Report's target.
         let report = Report::for_target(&qe_id, &[0; 64]);
 
-        // send attestation report back to daemon
+        // This enclave sends its attestation Report back to the attestation 
+        // daemon.
         stream.write(&report.as_ref()).unwrap();
         println!("Successfully sent report to daemon.");
     }

2e8c68338bf177cd94c73632f48cd6c67f27a78e
Lily S <lsturman@redhat.com>
2019-09-16 10:59:37 -0400

    still works

--- intel-sgx/attestation-tenant/src/main.rs
+++ intel-sgx/attestation-tenant/src/main.rs
@@ -1,6 +1,8 @@
-use failure::Error;
+mod cert_chain;
+mod sig;
 use ascii::AsciiStr;
 use dcap_ql::quote::*;
+use failure::Error;
 use openssl::{
     bn::BigNum,
     ec::{EcGroup, EcKey},
@@ -16,167 +18,87 @@ use openssl::{
 use percent_encoding::percent_decode;
 use std::{
     borrow::Borrow,
-    iter::Iterator,
     convert::TryFrom,
     env, fs,
     io::{Read, Write},
+    iter::Iterator,
     net::TcpStream,
 };
 
-
-#[derive(Clone)]
-pub struct CertChain {
-    chain : Vec<X509>,
-    max_len : usize
-}
-
-impl Default for CertChain {
-     fn default() -> Self {
-            CertChain {
-                chain: Vec::new(),
-                max_len : 10,
-        }
-     }
-}
-
-impl CertChain {
-    pub fn set_chain(&mut self, v: Vec<X509>) {
-        self.chain = v;
-    }
-
-    pub fn set_max_len(&mut self, len: usize) -> () {
-        self.max_len = len;
-    }
-    
-    pub fn len_ok(self) -> () {
-        assert!(self.chain.len() <= self.max_len)
-    }
-
-    pub fn pop_root(&mut self) -> X509 {
-        self.chain.pop().unwrap()
-    }
-
-    pub fn verify_issuers(self) -> () {
-        let mut iter = self.chain.iter().peekable();
-        while let Some(cert) = iter.next() {
-            let parent = iter.peek();
-            if parent.is_none() { continue };
-            assert_eq!(parent.unwrap().issued(&cert), X509VerifyResult::OK);                
-        println!("Issuer relationships in PCK cert chain are valid...");
-        }
-    }
-
-    pub fn verify_sigs(mut self, pck_cert: &X509) -> () {
-        // Parse out root cert, which will be at end of chain
-        // The rest of the chain holds intermediate certs
-        let root_cert = self.pop_root();
-
-        // Only the root certificate is added to the trusted store.
-        let mut store_bldr = store::X509StoreBuilder::new().unwrap();
-        store_bldr.add_cert(root_cert.clone()).unwrap();
-        let store = store_bldr.build();
-        
-        // Creates the chain of untrusted certificates.
-        let mut chain = Stack::new().unwrap();
-        for c in self.chain.iter() {
-            let _ = chain.push(c.clone());
-        }
-
-        // This context will be initialized with the trusted store and
-        // the chain of untrusted certificates to verify the leaf.
-        let mut context = X509StoreContext::new().unwrap();
-
-        // This operation verifies the leaf (PCK_cert) in the context of the
-        // chain. If the chain cannot be verified, the leaf will not be
-        // verified.
-        assert!(context
-            .init(&store, &pck_cert, &chain, |c| c.verify_cert())
-            .unwrap());
-
-        println!("Signatures on certificate chain are valid...");
-    }
-}
-
-#[derive(Copy, Clone)]
-pub struct Signature {
-    r: [u8; 32],
-    s: [u8; 32],
-}
-
-impl std::fmt::Debug for Signature {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "Signature {{ r: {:?}, s: {:?} }}",
-            self.r.iter(),
-            self.s.iter()
-        )
-    }
-}
-
-impl Eq for Signature {}
-impl PartialEq for Signature {
-    fn eq(&self, other: &Signature) -> bool {
-        self.r[..] == other.r[..] && self.s[..] == other.s[..]
-    }
-}
-
-impl Default for Signature {
-    fn default() -> Self {
-        Signature {
-            r: [0u8; 32],
-            s: [0u8; 32],
-        }
-    }
-}
-
-// turns &[u8] into Signature
-impl TryFrom<&[u8]> for Signature { 
-    type Error = Error;
-    fn try_from(value: &[u8]) -> Result<Self> {
-        let mut r: [u8; 32] = Default::default();
-        let mut s: [u8; 32] = Default::default();
-        r.copy_from_slice(&value[0..32]);
-        s.copy_from_slice(&value[32..64]);
-
-        Ok(Signature {
-            r: r,
-            s: s,
-        })
-    }
-}
-
-// turns Signature into ecdsa
-impl TryFrom<&Signature> for EcdsaSig {
-    type Error = Error;
-    fn try_from(value: &Signature) -> Result<Self> {
-        let r = BigNum::from_slice(&value.r).unwrap();
-        let s = BigNum::from_slice(&value.s).unwrap();
-        Ok(EcdsaSig::from_private_components(r, s)?)
-    }
-}
-
-// turns a Signature in to an ECDSA DER Vector
-impl TryFrom<&Signature> for Vec<u8> {
-    type Error = Error;
-    fn try_from(value: &Signature) -> Result<Self> {
-        Ok(EcdsaSig::try_from(value)?.to_der()?)
-    }
-}
+//#[derive(Copy, Clone)]
+//pub struct Signature {
+//    r: [u8; 32],
+//    s: [u8; 32],
+//}
+//
+//impl std::fmt::Debug for Signature {
+//    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//        write!(
+//            f,
+//            "Signature {{ r: {:?}, s: {:?} }}",
+//            self.r.iter(),
+//            self.s.iter()
+//        )
+//    }
+//}
+//
+//impl Eq for Signature {}
+//impl PartialEq for Signature {
+//    fn eq(&self, other: &Signature) -> bool {
+//        self.r[..] == other.r[..] && self.s[..] == other.s[..]
+//    }
+//}
+//
+//impl Default for Signature {
+//    fn default() -> Self {
+//        Signature {
+//            r: [0u8; 32],
+//            s: [0u8; 32],
+//        }
+//    }
+//}
+//
+//// turns &[u8] into Signature
+//impl TryFrom<&[u8]> for Signature {
+//    type Error = Error;
+//    fn try_from(value: &[u8]) -> Result<Self> {
+//        let mut r: [u8; 32] = Default::default();
+//        let mut s: [u8; 32] = Default::default();
+//        r.copy_from_slice(&value[0..32]);
+//        s.copy_from_slice(&value[32..64]);
+//
+//        Ok(Signature { r: r, s: s })
+//    }
+//}
+//
+//// turns Signature into ecdsa
+//impl TryFrom<&Signature> for EcdsaSig {
+//    type Error = Error;
+//    fn try_from(value: &Signature) -> Result<Self> {
+//        let r = BigNum::from_slice(&value.r).unwrap();
+//        let s = BigNum::from_slice(&value.s).unwrap();
+//        Ok(EcdsaSig::from_private_components(r, s)?)
+//    }
+//}
+//
+//// turns a Signature in to an ECDSA DER Vector
+//impl TryFrom<&Signature> for Vec<u8> {
+//    type Error = Error;
+//    fn try_from(value: &Signature) -> Result<Self> {
+//        Ok(EcdsaSig::try_from(value)?.to_der()?)
+//    }
+//}
 
 //#[derive(Borrow)]
 pub struct Key {
-    x_coord: [u8; 32],
-    y_coord: [u8; 32],
     pkey: PKey<Public>,
 }
 
 impl Key {
-
     pub fn new_from_xy(xy_coords: &[u8]) -> Self {
         let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
-        let mut x : [u8; 32] = Default::default();
-        let mut y : [u8; 32] = Default::default();
+        let mut x: [u8; 32] = Default::default();
+        let mut y: [u8; 32] = Default::default();
         x.copy_from_slice(&xy_coords[0..32]);
         y.copy_from_slice(&xy_coords[32..64]);
         let xbn = BigNum::from_slice(&x).unwrap();
@@ -185,19 +107,11 @@ impl Key {
         assert!(ec_key.check_key().is_ok());
         let pkey = PKey::from_ec_key(ec_key).unwrap();
 
-        Key {
-            x_coord: x,
-            y_coord: y,
-            pkey: pkey,
-        }
+        Key { pkey: pkey }
     }
 
     pub fn new_from_pubkey(pkey: PKey<Public>) -> Self {
-        Key {
-            x_coord: Default::default(),
-            y_coord: Default::default(),
-            pkey: pkey,
-        }
+        Key { pkey: pkey }
     }
 
     pub fn verify_sig(&self, signed: &[u8], sig: &Vec<u8>) -> () {
@@ -206,29 +120,14 @@ impl Key {
         assert!(verifier.verify(sig).unwrap());
     }
 
+    // This verifies the SHA-256 hash of the Attestation Public Key || QEAuthData
+    // (embedded in Quote, signed by PCK).
     pub fn verify_hash(&self, hashed_data: &[u8], unhashed_data: Vec<u8>) -> () {
         let mut hasher = sha::Sha256::new();
         hasher.update(&unhashed_data);
         let hash = hasher.finish();
         assert!(hash == hashed_data);
     }
-}
-
-
-// This verifies the SHA-256 hash of the Attestation Public Key || QEAuthData
-// (embedded in Quote, signed by PCK).
-fn verify_pck_hash(qe_report_body: &[u8], ak_pub: &[u8], qe_auth_data: &[u8]) {
-    let hashed_reportdata = &qe_report_body[320..352];
-    let mut unhashed = Vec::new();
-    unhashed.extend(ak_pub.to_vec());
-    unhashed.extend(qe_auth_data.to_vec());
-
-    let mut hasher = sha::Sha256::new();
-    hasher.update(&unhashed);
-    let hash = hasher.finish();
-
-    assert!(hash == hashed_reportdata);
-    println!("QE Report's hash is valid....");
 }
 
 fn main() {
@@ -243,7 +142,7 @@ fn main() {
     let mut stream_quote = stream.try_clone().expect("clone failed...");
     stream_quote.read_exact(&mut quote).unwrap();
 
-    // The ISV enclave report signature's signed material is the first 432 bytes 
+    // The ISV enclave report signature's signed material is the first 432 bytes
     // of the Quote. This is what the Quoting Enclave's Attestation Key signed.
     let ak_signed_material = &quote[0..432].to_vec();
 
@@ -279,12 +178,12 @@ fn main() {
         panic!("You must supply the path of a valid PCK certificate chain as the first argument.");
     }
     let cert_chain_file = env::args().nth(1).unwrap();
-    let cert_chain_contents = fs::read_to_string(&cert_chain_file[..]).unwrap();
-    let pck_cert_chain = X509::stack_from_pem(cert_chain_contents.as_bytes()).unwrap();
+    let cert_chain_file_contents = fs::read_to_string(&cert_chain_file[..]).unwrap();
+    let pck_cert_chain = X509::stack_from_pem(cert_chain_file_contents.as_bytes()).unwrap();
     println!("Tenant's PCK cert chain loaded...");
 
-    // This verifies the PCK certificate chain issuers and signatures. 
-    let mut cert_chain = CertChain::default();
+    // This verifies the PCK certificate chain issuers and signatures.
+    let mut cert_chain = cert_chain::CertChain::default();
     cert_chain.set_chain(pck_cert_chain.clone());
     cert_chain.clone().len_ok();
     cert_chain.clone().verify_issuers();
@@ -293,16 +192,18 @@ fn main() {
 
     // This verifies the Attestation Key's signature on the Quote.
     let attestation_key = Key::new_from_xy(&q_att_key_pub);
-    let quote_signature = Signature::try_from(q_enclave_report_sig).unwrap();
-    let quote_signature = Vec::try_from(&quote_signature).unwrap();
+    let quote_signature =
+        Vec::try_from(&sig::Signature::try_from(q_enclave_report_sig).unwrap()).unwrap();
     attestation_key.verify_sig(&ak_signed_material, &quote_signature);
     println!("AK signature on Quote header || report body is valid...");
 
     // This verifies the PCK's signature on the Attestation Public Key.
     let pc_key = Key::new_from_pubkey(quote_leaf_cert.public_key().unwrap());
-    let qe_report_signature = Signature::try_from(q_qe_report_sig).unwrap();
-    let qe_report_signature = Vec::try_from(&qe_report_signature).unwrap();
-    pc_key.borrow().verify_sig(&q_qe_report, &qe_report_signature);
+    let qe_report_signature =
+        Vec::try_from(&sig::Signature::try_from(q_qe_report_sig).unwrap()).unwrap();
+    pc_key
+        .borrow()
+        .verify_sig(&q_qe_report, &qe_report_signature);
     println!("PCK signature on AK is valid...");
 
     // This verifies that the hashed material signed by the PCK is correct.
@@ -311,7 +212,9 @@ fn main() {
     let mut unhashed_data = Vec::new();
     unhashed_data.extend(q_att_key_pub.to_vec());
     unhashed_data.extend(q_auth_data.to_vec());
-    pc_key.borrow().verify_hash(hashed_reportdata, unhashed_data);
+    pc_key
+        .borrow()
+        .verify_hash(hashed_reportdata, unhashed_data);
     println!("QE Report's hash is valid....");
 
     println!("\nQuote verified.");

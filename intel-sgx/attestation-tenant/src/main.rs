68febe060e31a2d1830997a3a37a29b767fef8ee
Lily S <lsturman@redhat.com>
2019-09-16 11:39:43 -0400

    small fixes with modules

--- intel-sgx/attestation-tenant/src/main.rs
+++ intel-sgx/attestation-tenant/src/main.rs
@@ -1,6 +1,7 @@
 mod cert_chain;
 mod sig;
 mod key;
+
 use ascii::AsciiStr;
 use dcap_ql::quote::*;
 use openssl::x509::*;
@@ -14,57 +15,16 @@ use std::{
     net::TcpStream,
 };
 
-////#[derive(Borrow)]
-//pub struct Key {
-//    pkey: PKey<Public>,
-//}
-//
-//impl Key {
-//    pub fn new_from_xy(xy_coords: &[u8]) -> Self {
-//        let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
-//        let mut x: [u8; 32] = Default::default();
-//        let mut y: [u8; 32] = Default::default();
-//        x.copy_from_slice(&xy_coords[0..32]);
-//        y.copy_from_slice(&xy_coords[32..64]);
-//        let xbn = BigNum::from_slice(&x).unwrap();
-//        let ybn = BigNum::from_slice(&y).unwrap();
-//        let ec_key = EcKey::from_public_key_affine_coordinates(&group, &xbn, &ybn).unwrap();
-//        assert!(ec_key.check_key().is_ok());
-//        let pkey = PKey::from_ec_key(ec_key).unwrap();
-//
-//        Key { pkey: pkey }
-//    }
-//
-//    pub fn new_from_pubkey(pkey: PKey<Public>) -> Self {
-//        Key { pkey: pkey }
-//    }
-//
-//    pub fn verify_sig(&self, signed: &[u8], sig: &Vec<u8>) -> () {
-//        let mut verifier = Verifier::new(MessageDigest::sha256(), &self.pkey).unwrap();
-//        verifier.update(signed).unwrap();
-//        assert!(verifier.verify(sig).unwrap());
-//    }
-//
-//    // This verifies the SHA-256 hash of the Attestation Public Key || QEAuthData
-//    // (embedded in Quote, signed by PCK).
-//    pub fn verify_hash(&self, hashed_data: &[u8], unhashed_data: Vec<u8>) -> () {
-//        let mut hasher = sha::Sha256::new();
-//        hasher.update(&unhashed_data);
-//        let hash = hasher.finish();
-//        assert!(hash == hashed_data);
-//    }
-//}
-
 fn main() {
     // The tenant requests attestation from the platform's attestation daemon.
-    let mut stream = TcpStream::connect("localhost:1034").unwrap();
-    let req = b"Request attestation"; // arbitrary init signal
-    stream.write(req).unwrap();
+    let mut stream_daemon = TcpStream::connect("localhost:1034").unwrap();
+    let req = b"Request attestation";
+    stream_daemon.write(req).unwrap();
 
     // The tenant receives a Quote of known length from the platform's attestation
     // daemon. This Quote verifies the enclave's measurement from its Report.
     let mut quote: [u8; 4702] = [0; 4702];
-    let mut stream_quote = stream.try_clone().expect("clone failed...");
+    let mut stream_quote = stream_daemon.try_clone().unwrap();
     stream_quote.read_exact(&mut quote).unwrap();
 
     // The ISV enclave report signature's signed material is the first 432 bytes
@@ -82,8 +42,6 @@ fn main() {
 
     // This gets individual certificates from the Quote's PCK chain.
     let quote_leaf_cert = &quote_pck_cert_chain[0];
-    let _quote_intermed_cert = &quote_pck_cert_chain[1];
-    let _quote_root_cert = &quote_pck_cert_chain[2];
 
     // This makes the Quote parseable and returns the Quote's signature section.
     let quote = dcap_ql::quote::Quote::parse(&quote).unwrap();

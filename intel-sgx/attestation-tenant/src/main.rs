1a797e8440d87c9267e4080eb419083d85b71e0f
Lily S <lsturman@redhat.com>
2019-09-16 11:13:22 -0400

    works with modules

--- intel-sgx/attestation-tenant/src/main.rs
+++ intel-sgx/attestation-tenant/src/main.rs
@@ -1,20 +1,9 @@
 mod cert_chain;
 mod sig;
+mod key;
 use ascii::AsciiStr;
 use dcap_ql::quote::*;
-use failure::Error;
-use openssl::{
-    bn::BigNum,
-    ec::{EcGroup, EcKey},
-    ecdsa::EcdsaSig,
-    hash::MessageDigest,
-    nid::Nid,
-    pkey::{PKey, Public},
-    sha,
-    sign::Verifier,
-    stack::Stack,
-    x509::*,
-};
+use openssl::x509::*;
 use percent_encoding::percent_decode;
 use std::{
     borrow::Borrow,
@@ -25,110 +14,46 @@ use std::{
     net::TcpStream,
 };
 
-//#[derive(Copy, Clone)]
-//pub struct Signature {
-//    r: [u8; 32],
-//    s: [u8; 32],
+////#[derive(Borrow)]
+//pub struct Key {
+//    pkey: PKey<Public>,
 //}
 //
-//impl std::fmt::Debug for Signature {
-//    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//        write!(
-//            f,
-//            "Signature {{ r: {:?}, s: {:?} }}",
-//            self.r.iter(),
-//            self.s.iter()
-//        )
+//impl Key {
+//    pub fn new_from_xy(xy_coords: &[u8]) -> Self {
+//        let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
+//        let mut x: [u8; 32] = Default::default();
+//        let mut y: [u8; 32] = Default::default();
+//        x.copy_from_slice(&xy_coords[0..32]);
+//        y.copy_from_slice(&xy_coords[32..64]);
+//        let xbn = BigNum::from_slice(&x).unwrap();
+//        let ybn = BigNum::from_slice(&y).unwrap();
+//        let ec_key = EcKey::from_public_key_affine_coordinates(&group, &xbn, &ybn).unwrap();
+//        assert!(ec_key.check_key().is_ok());
+//        let pkey = PKey::from_ec_key(ec_key).unwrap();
+//
+//        Key { pkey: pkey }
+//    }
+//
+//    pub fn new_from_pubkey(pkey: PKey<Public>) -> Self {
+//        Key { pkey: pkey }
+//    }
+//
+//    pub fn verify_sig(&self, signed: &[u8], sig: &Vec<u8>) -> () {
+//        let mut verifier = Verifier::new(MessageDigest::sha256(), &self.pkey).unwrap();
+//        verifier.update(signed).unwrap();
+//        assert!(verifier.verify(sig).unwrap());
+//    }
+//
+//    // This verifies the SHA-256 hash of the Attestation Public Key || QEAuthData
+//    // (embedded in Quote, signed by PCK).
+//    pub fn verify_hash(&self, hashed_data: &[u8], unhashed_data: Vec<u8>) -> () {
+//        let mut hasher = sha::Sha256::new();
+//        hasher.update(&unhashed_data);
+//        let hash = hasher.finish();
+//        assert!(hash == hashed_data);
 //    }
 //}
-//
-//impl Eq for Signature {}
-//impl PartialEq for Signature {
-//    fn eq(&self, other: &Signature) -> bool {
-//        self.r[..] == other.r[..] && self.s[..] == other.s[..]
-//    }
-//}
-//
-//impl Default for Signature {
-//    fn default() -> Self {
-//        Signature {
-//            r: [0u8; 32],
-//            s: [0u8; 32],
-//        }
-//    }
-//}
-//
-//// turns &[u8] into Signature
-//impl TryFrom<&[u8]> for Signature {
-//    type Error = Error;
-//    fn try_from(value: &[u8]) -> Result<Self> {
-//        let mut r: [u8; 32] = Default::default();
-//        let mut s: [u8; 32] = Default::default();
-//        r.copy_from_slice(&value[0..32]);
-//        s.copy_from_slice(&value[32..64]);
-//
-//        Ok(Signature { r: r, s: s })
-//    }
-//}
-//
-//// turns Signature into ecdsa
-//impl TryFrom<&Signature> for EcdsaSig {
-//    type Error = Error;
-//    fn try_from(value: &Signature) -> Result<Self> {
-//        let r = BigNum::from_slice(&value.r).unwrap();
-//        let s = BigNum::from_slice(&value.s).unwrap();
-//        Ok(EcdsaSig::from_private_components(r, s)?)
-//    }
-//}
-//
-//// turns a Signature in to an ECDSA DER Vector
-//impl TryFrom<&Signature> for Vec<u8> {
-//    type Error = Error;
-//    fn try_from(value: &Signature) -> Result<Self> {
-//        Ok(EcdsaSig::try_from(value)?.to_der()?)
-//    }
-//}
-
-//#[derive(Borrow)]
-pub struct Key {
-    pkey: PKey<Public>,
-}
-
-impl Key {
-    pub fn new_from_xy(xy_coords: &[u8]) -> Self {
-        let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
-        let mut x: [u8; 32] = Default::default();
-        let mut y: [u8; 32] = Default::default();
-        x.copy_from_slice(&xy_coords[0..32]);
-        y.copy_from_slice(&xy_coords[32..64]);
-        let xbn = BigNum::from_slice(&x).unwrap();
-        let ybn = BigNum::from_slice(&y).unwrap();
-        let ec_key = EcKey::from_public_key_affine_coordinates(&group, &xbn, &ybn).unwrap();
-        assert!(ec_key.check_key().is_ok());
-        let pkey = PKey::from_ec_key(ec_key).unwrap();
-
-        Key { pkey: pkey }
-    }
-
-    pub fn new_from_pubkey(pkey: PKey<Public>) -> Self {
-        Key { pkey: pkey }
-    }
-
-    pub fn verify_sig(&self, signed: &[u8], sig: &Vec<u8>) -> () {
-        let mut verifier = Verifier::new(MessageDigest::sha256(), &self.pkey).unwrap();
-        verifier.update(signed).unwrap();
-        assert!(verifier.verify(sig).unwrap());
-    }
-
-    // This verifies the SHA-256 hash of the Attestation Public Key || QEAuthData
-    // (embedded in Quote, signed by PCK).
-    pub fn verify_hash(&self, hashed_data: &[u8], unhashed_data: Vec<u8>) -> () {
-        let mut hasher = sha::Sha256::new();
-        hasher.update(&unhashed_data);
-        let hash = hasher.finish();
-        assert!(hash == hashed_data);
-    }
-}
 
 fn main() {
     // The tenant requests attestation from the platform's attestation daemon.
@@ -191,14 +116,14 @@ fn main() {
     println!("PCK cert chain verified...");
 
     // This verifies the Attestation Key's signature on the Quote.
-    let attestation_key = Key::new_from_xy(&q_att_key_pub);
+    let attestation_key = key::Key::new_from_xy(&q_att_key_pub);
     let quote_signature =
         Vec::try_from(&sig::Signature::try_from(q_enclave_report_sig).unwrap()).unwrap();
     attestation_key.verify_sig(&ak_signed_material, &quote_signature);
     println!("AK signature on Quote header || report body is valid...");
 
     // This verifies the PCK's signature on the Attestation Public Key.
-    let pc_key = Key::new_from_pubkey(quote_leaf_cert.public_key().unwrap());
+    let pc_key = key::Key::new_from_pubkey(quote_leaf_cert.public_key().unwrap());
     let qe_report_signature =
         Vec::try_from(&sig::Signature::try_from(q_qe_report_sig).unwrap()).unwrap();
     pc_key

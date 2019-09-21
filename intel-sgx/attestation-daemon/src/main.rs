4ba9a13f1a16e3f7418bff1df39671b00e8279e2
Lily S <lsturman@redhat.com>
2019-09-21 18:25:48 -0400

    serialization worked

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use serde_json::*;
 
 fn main() {
     println!("Daemon listening for attestation request on port 1034... ");
@@ -16,14 +17,25 @@ fn main() {
         // sends the Quoting Enclave's Target Info to the enclave. This Target Info will be
         // used as the target for the enclave's attestation Report.
         let qe_ti = dcap_ql::target_info().expect("Could not retrieve QE target info.");
-        enclave_cnx.write(&qe_ti.as_ref()).unwrap();
+        let serialized_qe_ti = serde_json::to_string(&qe_ti).unwrap();
+        println!("serialized qe ti len: {:?}", serialized_qe_ti.len());
+
+        enclave_cnx.write(&serialized_qe_ti.as_bytes()).unwrap();
+        //enclave_cnx.write(&qe_ti.as_ref()).unwrap();
 
         // The attestation daemon receives the Report back from the attesting enclave.
-        let mut report_buf = [0; sgx_isa::Report::UNPADDED_SIZE];
+        //let mut report_buf = [0; sgx_isa::Report::UNPADDED_SIZE];
+        let mut report_buf = [0; 694];
+        
         enclave_cnx
             .read_exact(&mut report_buf)
             .expect("Could not read report");
-        let report = sgx_isa::Report::try_copy_from(&report_buf).unwrap();
+
+        let s = String::from_utf8(report_buf.to_vec()).unwrap();
+
+        let report : sgx_isa::Report = serde_json::from_str(&s).unwrap();
+
+        //let report = sgx_isa::Report::try_copy_from(&report_buf).unwrap();
 
         // The attestation daemon gets a Quote from the Quoting Enclave for the Report. 
         // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating 
--- intel-sgx/attestation-enclave/src/main.rs
+++ intel-sgx/attestation-enclave/src/main.rs
@@ -13,9 +13,17 @@ fn main() {
         // attestation daemon, in the form of a TargetInfo structure. The 
         // TargetInfo contains the measurement and attribute flags of the
         // Quoting Enclave.
-        let mut buf = [0; Targetinfo::UNPADDED_SIZE];
+        
+        //let mut buf = [0; Targetinfo::UNPADDED_SIZE];
+        
+        let mut buf = [0; 196];
         stream.read_exact(&mut buf).unwrap();
-        let qe_id = Targetinfo::try_copy_from(&buf).unwrap();
+
+        let s = String::from_utf8(buf.to_vec()).unwrap();
+
+        let qe_id : sgx_isa::Targetinfo = serde_json::from_str(&s).unwrap();
+
+        //let qe_id = Targetinfo::try_copy_from(&buf).unwrap();
 
         // This enclave creates a Report attesting its identity, with the Quoting
         // Enclave as the Report's target.
@@ -23,7 +31,13 @@ fn main() {
 
         // This enclave sends its attestation Report back to the attestation 
         // daemon.
-        stream.write(&report.as_ref()).unwrap();
+        
+        let serialized_report = serde_json::to_string(&report).unwrap();
+        println!("serialized report len: {}", serialized_report.as_bytes().len());
+
+
+        //stream.write(&report.as_ref()).unwrap();
+        stream.write(&serialized_report.as_bytes()).unwrap();
         println!("Successfully sent report to daemon.");
     }
 }

542bd5432de7a7586e4a9877ac2b1843bf6912f2
Lily S <lsturman@redhat.com>
2019-09-21 19:59:48 -0400

    done, works, is good

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -6,13 +6,14 @@ const ENCLAVE_CONN: &'static str = "localhost:1032";
 const SER_REPORT_SIZE: usize = 699;
 
 fn main() {
-    println!("Daemon listening for attestation request on port 1034... ");
+    println!(
+        "Daemon listening for attestation request on {}... ",
+        LISTENER_CONN
+    );
 
     // The attestation daemon handles each incoming connection from a tenant. The tenant, by
     // connecting, is requesting an attestation of the enclave.
     for incoming_tenant_stream in TcpListener::bind(LISTENER_CONN).unwrap().incoming() {
-
-
         // The attestation daemon retrieves the Quoting Enclave's Target Info and
         // sends the Quoting Enclave's Target Info to the enclave. This Target Info will be
         // used as the target for the enclave's attestation Report.
@@ -22,22 +23,22 @@ fn main() {
         enclave_stream.write(&serialized_qe_ti.as_bytes()).unwrap();
 
         // The attestation daemon receives the Report back from the attesting enclave.
-        let mut report_buf = [0; SER_REPORT_SIZE]; 
+        let mut report_buf = [0; SER_REPORT_SIZE];
         enclave_stream
             .read_exact(&mut report_buf)
             .expect("Could not read report");
         let serialized_report = String::from_utf8(report_buf.to_vec()).unwrap();
-        let report : sgx_isa::Report = serde_json::from_str(&serialized_report).unwrap();
+        let report: sgx_isa::Report = serde_json::from_str(&serialized_report).unwrap();
 
-        // The attestation daemon gets a Quote from the Quoting Enclave for the Report. 
-        // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating 
+        // The attestation daemon gets a Quote from the Quoting Enclave for the Report.
+        // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating
         // the Quote. The Quote is signed with the Quoting Enclave's Attestation Key.
         let quote = dcap_ql::quote(&report).expect("Could not generate quote.");
 
         // The attestation daemon sends the Quote to the tenant.
         let mut tenant_stream = incoming_tenant_stream.unwrap();
         tenant_stream.write(&quote).unwrap();
-        
+
         println!("\nQuote successfully generated and sent to tenant...");
     }
 }
--- intel-sgx/attestation-tenant/src/main.rs
+++ intel-sgx/attestation-tenant/src/main.rs
@@ -2,10 +2,10 @@ mod cert_chain;
 mod key;
 mod sig;
 
-use percent_encoding::percent_decode;
 use bufstream::BufStream;
 use dcap_ql::quote::*;
 use openssl::x509::*;
+use percent_encoding::percent_decode;
 use std::{
     borrow::Borrow,
     convert::TryFrom,
@@ -15,72 +15,88 @@ use std::{
     net::TcpStream,
 };
 
+const DAEMON_CONN: &'static str = "localhost:1034";
+const VEC_QUOTE_SIZE: usize = 4702;
+const AK_SIGNED_MATERIAL_LEN: usize = 432;
+const CERT_DATA_START_OFFSET: usize = 1052;
+const HASHED_REPORT_DATA_START: usize = 320;
+const HASHED_REPORT_DATA_END: usize = 352;
+
+// The tenant requests attestation of an enclave from the platform's attestation daemon, and
+// receives a Quote from the daemon. The Quote verifies the enclave's measurement. The tenant
+// verifies:
+// 1. That the Quote's PCK Certificate (embedded in the Cert Data) is valid.
+// 2. That the PCK Certificate's Key signed the platform's Attestation Key.
+// 3. That the Attestation Key signed the Quote.
+// 4. That the hashed material (containing the Attestation Key) signed by the PCK is valid.
+
 fn main() {
     // The tenant requests attestation from the platform's attestation daemon.
-    let daemon_conn = TcpStream::connect("localhost:1034").unwrap();
+    // The actual signal is arbitrary.
+    let daemon_conn = TcpStream::connect(DAEMON_CONN).unwrap();
     let mut daemon_buf = BufStream::new(daemon_conn);
     daemon_buf.write(&b"Request attestation"[..]).unwrap();
 
-    // The tenant receives a Quote of known length from the platform's attestation
-    // daemon. This Quote verifies the enclave's measurement from its Report.
-    let mut quote: [u8; 4702] = [0; 4702];
-    //let mut quote: [u8; 4266] = [0; 4266];
+    // The tenant receives a Quote from the platform's attestation
+    // daemon. This Quote verifies the enclave's self-measurement in its Report.
+    let mut quote = [0u8; VEC_QUOTE_SIZE];
     daemon_buf.read_exact(&mut quote).unwrap();
 
     // The Quoting Enclave's Attestation Key signed the Quote Header (Quote bytes 0-48)
     // concatenated with the ISV Enclave Report (Quote bytes 49-432).
-    let ak_signed_material = &quote[0..432].to_vec();
+    let att_key_signed_material = &quote[0..AK_SIGNED_MATERIAL_LEN].to_vec();
 
-    let cert_data = &quote[1052..].to_vec();
+    // The Quote's Certification Data contains the PCK Cert Chain and PCK Certificate,
+    // which signs the Attestation Key.
+    let cert_data = &quote[CERT_DATA_START_OFFSET..].to_vec();
     let cert_data_utf8_decoded = percent_decode(cert_data)
-                .decode_utf8()
-                .unwrap()
-                .into_owned();
-            let quote_pck_cert_chain =
-                X509::stack_from_pem(&cert_data_utf8_decoded.as_bytes()[..]).unwrap();
-            let quote_leaf_cert = &quote_pck_cert_chain[0];
-
-    // This makes the Quote parseable and returns the Quote's signature section.
-    let quote = dcap_ql::quote::Quote::parse(&quote).unwrap();
-    let q_sig = quote.signature::<Quote3SignatureEcdsaP256>().unwrap();
+        .decode_utf8()
+        .unwrap()
+        .into_owned();
+    let quote_pck_cert_chain =
+        X509::stack_from_pem(&cert_data_utf8_decoded.as_bytes()[..]).unwrap();
+    let quote_pck_leaf_cert = &quote_pck_cert_chain[0];
 
     // This parses the Quote's signature section.
+    let quote = dcap_ql::quote::Quote::parse(&quote).unwrap();
+    let q_sig = quote.signature::<Quote3SignatureEcdsaP256>().unwrap();
     let q_enclave_report_sig = q_sig.signature();
     let q_qe_report = q_sig.qe3_report();
     let q_qe_report_sig = q_sig.qe3_signature();
     let q_att_key_pub = q_sig.attestation_public_key();
     let q_auth_data = q_sig.authentication_data();
 
-    // The tenant's PCK certificate chain is loaded.
+    // The tenant's PCK certificate chain must be loaded to verify the Quote's PCK Leaf Certificate.
+    // The root certificate in this chain is trusted, since it is provided by the tenant.
     let cert_chain_file = env::args()
         .nth(1)
         .expect("You must supply the path of a valid PCK certificate chain as the first argument.");
     let cert_chain_file_contents =
         fs::read_to_string(&cert_chain_file[..]).expect("PCK cert chain file path invalid.");
 
-    // This reconstructs the PCK chain with the Quote's leaf cert added to end of tenant's chain.
+    // The PCK chain is reconstructed with the Quote's leaf cert added to end of tenant's chain.
     let cert_chain = cert_chain::CertChain::new_from_chain(
         X509::stack_from_pem(cert_chain_file_contents.as_bytes()).unwrap(),
-        &quote_leaf_cert,
+        &quote_pck_leaf_cert,
     );
     cert_chain.len_ok();
     println!("Tenant's PCK cert chain loaded...");
 
-    // This verifies the PCK certificate chain issuers and signatures.
+    // The PCK certificate chain's issuers and signatures are verified.
     cert_chain.verify_issuers();
     cert_chain.verify_sigs();
     println!("PCK cert chain verified...");
 
-    // This verifies the Attestation Key's signature on the Quote.
+    // The Attestation Key's signature on the Quote is verified.
     let attestation_key = key::Key::new_from_xy(&q_att_key_pub);
     let quote_signature = sig::Signature::try_from(q_enclave_report_sig)
         .unwrap()
         .to_der_vec();
-    attestation_key.verify_sig(&ak_signed_material, &quote_signature);
+    attestation_key.verify_sig(&att_key_signed_material, &quote_signature);
     println!("AK signature on Quote header || report body is valid...");
 
-    // This verifies the PCK's signature on the Attestation Public Key.
-    let pc_key = key::Key::new_from_pubkey(quote_leaf_cert.public_key().unwrap());
+    // The PCK's signature on the Attestation Public Key is verified.
+    let pc_key = key::Key::new_from_pubkey(quote_pck_leaf_cert.public_key().unwrap());
     let qe_report_signature = sig::Signature::try_from(q_qe_report_sig)
         .unwrap()
         .to_der_vec();
@@ -90,9 +106,7 @@ fn main() {
     println!("PCK signature on AK is valid...");
 
     // This verifies that the hashed material signed by the PCK is correct.
-    //verify_pck_hash(&q_qe_report, &q_att_key_pub, &q_auth_data);
-    //let hashed_reportdata = &q_qe_report[320..352];
-    let hashed_reportdata = &q_qe_report[320..352];
+    let hashed_reportdata = &q_qe_report[HASHED_REPORT_DATA_START..HASHED_REPORT_DATA_END];
     let mut unhashed_data = Vec::new();
     unhashed_data.extend(q_att_key_pub.to_vec());
     unhashed_data.extend(q_auth_data.to_vec());

e8b19f9f1b6a82b823ecbbcf2529c0a2dccf4a08
Lily S <lsturman@redhat.com>
2019-09-21 07:57:20 -0400

    info not sending

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -1,23 +1,9 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use serde_json::*;
+use bufstream::BufStream;
 
 fn main() { 
-    let ti = sgx_isa::Targetinfo::default();
-
-    println!("raw ti: {:?}", ti);
-    let serialized = serde_json::to_string(&ti).unwrap();
-    println!("serialized: {}", serialized);
-    let deserialized : sgx_isa::Targetinfo = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized: {:?}", deserialized);
-    
-    let report = sgx_isa::Report::default();
-    println!("raw report: {:?}", report);
-    let serialized = serde_json::to_string(&report).unwrap();
-    println!("serialized: {}", serialized);
-    let deserialized : sgx_isa::Report = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized: {:?}", deserialized);
-
     println!("Daemon listening for attestation request on port 1034... ");
 
     // The attestation daemon handles each incoming connection from a tenant. The tenant, by
@@ -27,32 +13,30 @@ fn main() {
 
         // The attestation daemon connects to the enclave.
         let mut enclave_cnx = TcpStream::connect("localhost:1032").unwrap();
+        let mut enclave_buf = BufStream::new(enclave_cnx);
 
         // The attestation daemon retrieves the Quoting Enclave's Target Info and
         // sends the Quoting Enclave's Target Info to the enclave. This Target Info will be
-        // used as the target for the enclave's attestation Report.
-        
+        // used as the target for the enclave's attestation Report.        
         let qe_ti = dcap_ql::target_info().expect("Could not retrieve QE target info.");
         let serialized_qe_ti = serde_json::to_string(&qe_ti).unwrap();
-        let size = serialized_qe_ti.len();
-        println!("size: {}", size);
 
-        //enclave_cnx.write(&qe_ti.as_ref()).unwrap();
-        enclave_cnx.write(serialized_qe_ti.as_bytes()).unwrap();
+        enclave_buf.write(serialized_qe_ti.as_bytes()).unwrap();
+        println!("wrote ti to enclave");
 
-//        // The attestation daemon receives the Report back from the attesting enclave.
-//        let mut report_buf = [0; sgx_isa::Report::UNPADDED_SIZE];
-//        enclave_cnx
-//            .read_exact(&mut report_buf)
-//            .expect("Could not read report");
-//        let report = sgx_isa::Report::try_copy_from(&report_buf).unwrap();
-//
-//        // The attestation daemon gets a Quote from the Quoting Enclave for the Report. 
-//        // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating 
-//        // the Quote. The Quote is signed with the Quoting Enclave's Attestation Key.
-//        let quote = dcap_ql::quote(&report).expect("Could not generate quote.");
-//        println!("\nQuote successfully generated...");
-//
+        // The attestation daemon receives the Report back from the attesting enclave.
+        let mut buf_report = String::new();
+        enclave_buf.read_to_string(&mut buf_report).unwrap();
+        println!("read to string: {:?}", buf_report);
+
+        //let report : sgx_isa::Report = serde_json::from_str(&buf_report).unwrap();
+        
+        // The attestation daemon gets a Quote from the Quoting Enclave for the Report. 
+        // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating 
+        // the Quote. The Quote is signed with the Quoting Enclave's Attestation Key.
+        //let quote = dcap_ql::quote(&report).expect("Could not generate quote.");
+        //println!("\nQuote successfully generated...");
+
 //        // The attestation daemon sends the Quote to the tenant.
 //        stream_client.write(&quote).unwrap();
     }
--- intel-sgx/attestation-enclave/src/main.rs
+++ intel-sgx/attestation-enclave/src/main.rs
@@ -1,37 +1,42 @@
 use sgx_isa::{Report, Targetinfo};
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use serde_json::*;
+use bufstream::BufStream;
 
 fn main() {
     println!("\nListening on port 1032....\n");
 
     // This enclave handles each incoming connection from attestation daemon.
-    for stream in TcpListener::bind("localhost:1032").unwrap().incoming() {
-        let mut stream = stream.unwrap();
+    //for stream in TcpListener::bind("localhost:1032").unwrap().incoming() {
+        //let stream = TcpListener::bind("localhost:1032").unwrap().incoming();
+        let mut stream = TcpStream::connect("localhost:1032").unwrap();
+        //let mut stream = stream.unwrap();
+        let mut daemon_buf = BufStream::new(stream);
 
         // This enclave receives the identity of the Quoting Enclave from the
         // attestation daemon, in the form of a TargetInfo structure. The 
         // TargetInfo contains the measurement and attribute flags of the
         // Quoting Enclave.
-        //let mut buf = [0; Targetinfo::UNPADDED_SIZE];
-        //let mut buf = [0; 1146];
-        let mut buf = String::new();
-        
-        //stream.read_exact(&mut buf).unwrap();
-        stream.read_to_string(&mut buf).unwrap();
+        let mut buf_ti = String::new();
+        daemon_buf.read_to_string(&mut buf_ti).unwrap();
 
-        println!("read to string: {:?}", buf);
-        //let deserialized_buf = 
-        //let qe_id = Targetinfo::try_copy_from(&buf).unwrap();
+        println!("read to string: {:?}", buf_ti);
+        let qe_id: sgx_isa::Targetinfo = serde_json::from_str(&buf_ti).unwrap();
+        println!("deserialized = {:?}", qe_id);
 
         // This enclave creates a Report attesting its identity, with the Quoting
         // Enclave as the Report's target.
-        //let report = Report::for_target(&qe_id, &[0; 64]);
+        let report = Report::for_target(&qe_id, &[0; 64]);
+        println!("report: {:?}", report);
+
+        //let serialized_report = serde_json::to_string(&report).unwrap();
+        //println!("serialized report: {}", serialized_report);
+        //daemon_buf.write(serialized_report.as_bytes()).unwrap();
+
 
         // This enclave sends its attestation Report back to the attestation 
         // daemon.
-        //stream.write(&report.as_ref()).unwrap();
         //println!("Successfully sent report to daemon.");
-    }
+    //}
 }

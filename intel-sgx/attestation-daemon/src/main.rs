74373d8a65f8de2e6aaac2d57740aedbe5698b66
Lily S <lsturman@redhat.com>
2019-09-21 19:36:54 -0400

    still works

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -1,51 +1,43 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use serde_json::*;
+
+const LISTENER_CONN: &'static str = "localhost:1034";
+const ENCLAVE_CONN: &'static str = "localhost:1032";
+const SER_REPORT_SIZE: usize = 698;
 
 fn main() {
     println!("Daemon listening for attestation request on port 1034... ");
 
     // The attestation daemon handles each incoming connection from a tenant. The tenant, by
     // connecting, is requesting an attestation of the enclave.
-    for stream_client in TcpListener::bind("localhost:1034").unwrap().incoming() {
-        let mut stream_client = stream_client.unwrap();
+    for incoming_tenant_stream in TcpListener::bind(LISTENER_CONN).unwrap().incoming() {
 
-        // The attestation daemon connects to the enclave.
-        let mut enclave_cnx = TcpStream::connect("localhost:1032").unwrap();
 
         // The attestation daemon retrieves the Quoting Enclave's Target Info and
         // sends the Quoting Enclave's Target Info to the enclave. This Target Info will be
         // used as the target for the enclave's attestation Report.
         let qe_ti = dcap_ql::target_info().expect("Could not retrieve QE target info.");
         let serialized_qe_ti = serde_json::to_string(&qe_ti).unwrap();
-        //let serialized_qe_ti = serde_json::to_vec(&qe_ti).unwrap();
-        println!("serialized qe ti len: {:?}", serialized_qe_ti.len());
-
-        //enclave_cnx.write(&serialized_qe_ti[..]).unwrap();
-        enclave_cnx.write(&serialized_qe_ti.as_bytes()).unwrap();
-        //enclave_cnx.write(&qe_ti.as_ref()).unwrap();
+        let mut enclave_stream = TcpStream::connect(ENCLAVE_CONN).unwrap();
+        enclave_stream.write(&serialized_qe_ti.as_bytes()).unwrap();
 
         // The attestation daemon receives the Report back from the attesting enclave.
-        //let mut report_buf = [0; sgx_isa::Report::UNPADDED_SIZE];
-        let mut report_buf = [0; 698];
-        
-        enclave_cnx
+        let mut report_buf = [0; SER_REPORT_SIZE]; 
+        enclave_stream
             .read_exact(&mut report_buf)
             .expect("Could not read report");
-
-        let s = String::from_utf8(report_buf.to_vec()).unwrap();
-
-        let report : sgx_isa::Report = serde_json::from_str(&s).unwrap();
-
-        //let report = sgx_isa::Report::try_copy_from(&report_buf).unwrap();
+        let serialized_report = String::from_utf8(report_buf.to_vec()).unwrap();
+        let report : sgx_isa::Report = serde_json::from_str(&serialized_report).unwrap();
 
         // The attestation daemon gets a Quote from the Quoting Enclave for the Report. 
         // The Quoting Enclave verifies the Report's MAC as a prerequisite for generating 
         // the Quote. The Quote is signed with the Quoting Enclave's Attestation Key.
         let quote = dcap_ql::quote(&report).expect("Could not generate quote.");
-        println!("\nQuote successfully generated...");
 
         // The attestation daemon sends the Quote to the tenant.
-        stream_client.write(&quote).unwrap();
+        let mut tenant_stream = incoming_tenant_stream.unwrap();
+        tenant_stream.write(&quote).unwrap();
+        
+        println!("\nQuote successfully generated and sent to tenant...");
     }
 }

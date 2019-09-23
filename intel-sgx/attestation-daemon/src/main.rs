da589c832ab5e13ec55d32d5f51b1fed3ca80355
Lily S <lsturman@redhat.com>
2019-09-23 11:48:54 -0400

    sgx demo

--- intel-sgx/attestation-daemon/src/main.rs
+++ intel-sgx/attestation-daemon/src/main.rs
@@ -3,7 +3,7 @@ use std::net::{TcpListener, TcpStream};
 
 const LISTENER_CONN: &'static str = "localhost:1034";
 const ENCLAVE_CONN: &'static str = "localhost:1032";
-const SER_REPORT_SIZE: usize = 703;
+const SER_REPORT_SIZE: usize = 696;
 
 fn main() {
     println!(
